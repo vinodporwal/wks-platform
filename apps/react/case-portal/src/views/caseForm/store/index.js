65a647df73474594681f077a3aae56ce70f3fd70
Sandip Dhere <sandipdhere105@gmail.com>
2025-04-01 13:02:16 +0530

    Case document issue resolved.

--- apps/react/case-portal/src/views/caseForm/store/index.js
+++ apps/react/case-portal/src/views/caseForm/store/index.js
@@ -4,32 +4,56 @@ const CaseStore = {
   saveDocumentsFromFiles,
 }
 
-async function saveDocumentsFromFiles(
-  keycloak,
-  files,
-  businessKey,
-  progressCallback,
-) {
-  return Promise.all(
-    files.map((file) => {
+// async function saveDocumentsFromFiles(
+//   keycloak,
+//   files,
+//   businessKey,
+//   progressCallback,
+// ) {
+//   return Promise.all(
+//     files.map((file) => {
+//       const args = {
+//         dir: 'cases',
+//         file: file,
+//         keycloak,
+//         progress: (e, percent) => {
+//           progressCallback(percent)
+//         },
+//       }
+
+//       return FileService.upload(args)
+//         .then((data) => saveDocument(keycloak, businessKey, data))
+//         .catch(() => {
+//           return Promise.reject(
+//             `Could't upload this file "${file.name}", try again with other file.`,
+//           )
+//         })
+//     }),
+//   )
+// }
+
+async function saveDocumentsFromFiles(keycloak, files, businessKey, progressCallback) {
+  const results = [];
+  for (const file of files) {
       const args = {
         dir: 'cases',
         file: file,
         keycloak,
         progress: (e, percent) => {
-          progressCallback(percent)
+        progressCallback(percent);
         },
+    };
+    try {
+      const uploadData = await FileService.upload(args);
+      const savedDocument = await saveDocument(keycloak, businessKey, uploadData);
+      results.push(savedDocument);
+    } catch (error) {
+      throw new Error(`Couldn't upload this file "${file.name}", try again with another file.`);
       }
 
-      return FileService.upload(args)
-        .then((data) => saveDocument(keycloak, businessKey, data))
-        .catch(() => {
-          return Promise.reject(
-            `Could't upload this file "${file.name}", try again with other file.`,
-          )
-        })
-    }),
-  )
+  }
+
+  return results;
 }
 
 async function saveDocument(keycloak, businessKey, document) {
