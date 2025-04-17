78eafe20d13d5e863d12d19bb685952649a78cbf
Sandip Dhere <sandipdhere105@gmail.com>
2025-04-17 15:59:46 +0530

    UI changes

--- apps/react/case-portal/src/views/caseForm/caseForm.js
+++ apps/react/case-portal/src/views/caseForm/caseForm.js
@@ -1311,11 +1311,11 @@ export const CaseForm = ({ open, handleClose, aCase, keycloak }) => {
                 {'Print'}
               </Button>
 
-              <Button color='inherit' onClick={onSave}>
+              <Button hidden={aCase?.isDraft !== 'y'} color='inherit' onClick={onSave}>
                 {'Save'}
               </Button>
               {/* Case Actions Menu */}
-              <IconButton
+              {/* <IconButton
                 edge='end'
                 color='inherit'
                 onClick={handleMenuOpen}
@@ -1337,7 +1337,7 @@ export const CaseForm = ({ open, handleClose, aCase, keycloak }) => {
                     {t('pages.caseform.actions.startProcess')}
                   </MenuItem>
                 }
-              </Menu>
+              </Menu> */}
             </Toolbar>
           </AppBar>
 
@@ -1439,6 +1439,8 @@ export const CaseForm = ({ open, handleClose, aCase, keycloak }) => {
                           } else if (event.component.key === 'onSave') {
                             // onSubmitRecommendation()
                             onSave()
+                          }  else if (event.component.key === 'analysisSubmit') {
+                            onAnalysisSave()
                           }
                         }}
                       />
