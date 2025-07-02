d17ccf6af90f7fa3aeda24a7590078b064db4180
Sandip Dhere <sandip.dhere@honeywell.com>
2025-07-02 15:29:15 +0530

    Logs added.

--- apps/java/libraries/api-security/src/main/java/com/wks/api/security/utils/HttpUtils.java
+++ apps/java/libraries/api-security/src/main/java/com/wks/api/security/utils/HttpUtils.java
@@ -31,6 +31,8 @@ public final class HttpUtils {
 			}
 
 			URL allowedOrigin = new URL(url);
+			System.out.println("allowedOrigin "+ allowedOrigin);
+			System.out.println("allowedOrigin host "+ allowedOrigin.getHost());
 			return allowedOrigin.getHost();
 		} catch (MalformedURLException e) {
 			throw new IllegalArgumentException(e);
