e6a4c6d6cccde416dc0e607d8f58525c3f30e7cf
Shailenra <shailendra.c@iksula.com>
2025-07-10 12:28:29 +0530

    init

--- apps/react/case-portal/src/components/colums/MegColums.js
+++ apps/react/case-portal/src/components/colums/MegColums.js
@@ -554,3 +554,82 @@ export const ConsumptionAopMegColumns = [
     hidden: true,
   },
 ]
+export const ConfigurationMegColumns = [
+  {
+    field: 'Particulars',
+    title: 'Type',
+    editable: false,
+    width: 200,
+    hidden: true,
+  },
+  {
+    field: 'NormParametersId',
+    hidden: true,
+  },
+
+  {
+    field: 'productName',
+    title: 'Particulars',
+    editable: false,
+    width: 200,
+  },
+  {
+    field: 'UOM',
+    title: 'UOM / MT',
+    editable: false,
+    width: 150,
+  },
+  {
+    field: 'april',
+    title: 4,
+  },
+  {
+    field: 'may',
+    title: 5,
+  },
+  {
+    field: 'june',
+    title: 6,
+  },
+  {
+    field: 'july',
+    title: 7,
+  },
+  {
+    field: 'aug',
+    title: 8,
+  },
+  {
+    field: 'sep',
+    title: 9,
+  },
+  {
+    field: 'oct',
+    title: 10,
+  },
+  {
+    field: 'nov',
+    title: 11,
+  },
+  {
+    field: 'dec',
+    title: 12,
+  },
+  {
+    field: 'jan',
+    title: 1,
+  },
+  {
+    field: 'feb',
+    title: 2,
+  },
+  {
+    field: 'march',
+    title: 3,
+  },
+  {
+    field: 'isEditable',
+    title: 'isEditable',
+    hidden: true,
+  },
+]
