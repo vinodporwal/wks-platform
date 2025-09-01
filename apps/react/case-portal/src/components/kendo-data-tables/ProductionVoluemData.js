4181d24f133d398ee621b9d8511093a2933385a8
Ankit <ankitofficial95@gmail.com>
2025-09-01 22:32:40 +0530

    data service issue

--- apps/react/case-portal/src/components/kendo-data-tables/ProductionVoluemData.js
+++ apps/react/case-portal/src/components/kendo-data-tables/ProductionVoluemData.js
@@ -183,7 +183,7 @@ const ProductionvolumeData = ({ permissions }) => {
         setModifiedCells({})
 
         const responseForNorms =
-          await ProductionVolumeDataApiService.calculateNormsHistorianValues(
+          await DataService.calculateNormsHistorianValues(
             plantId,
             localStorage.getItem('year'),
             startDate,
