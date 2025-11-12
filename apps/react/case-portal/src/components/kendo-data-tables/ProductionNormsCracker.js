d78890e2dca86d848fa8f3d74f480697fd45040e
Ankit <ankitofficial95@gmail.com>
2025-11-12 12:16:43 +0530

    cracker monthwise production calculate btn enable

--- apps/react/case-portal/src/components/kendo-data-tables/ProductionNormsCracker.js
+++ apps/react/case-portal/src/components/kendo-data-tables/ProductionNormsCracker.js
@@ -183,6 +183,7 @@ const ProductionNormsCracker = ({ permissions }) => {
         // clear modified cells and refresh data
         setModifiedCellsC2C3R({})
         await fetchDataC2C3R()
+        await fetchData()
       } else {
         setSnackbarOpen(true)
         setSnackbarData({ message: 'Data Save Failed!', severity: 'error' })
