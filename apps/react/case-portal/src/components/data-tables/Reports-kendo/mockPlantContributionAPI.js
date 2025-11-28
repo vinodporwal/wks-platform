5fd078868502ec8ab7879ff61fdac49a616cc108
Ankit <ankitofficial95@gmail.com>
2025-11-28 20:29:07 +0530

    value formattor added for byProducts t21

--- apps/react/case-portal/src/components/data-tables/Reports-kendo/mockPlantContributionAPI.js
+++ apps/react/case-portal/src/components/data-tables/Reports-kendo/mockPlantContributionAPI.js
@@ -176,7 +176,7 @@ export const MockReportService = {
                       editable: false,
                       width: 120,
                       align: 'right',
-                      format: FORMAT_VALUES_3_DECIMAL,
+                      format: FORMAT_VALUES_NORMS,
 
                       type: 'number',
                     },
@@ -266,7 +266,6 @@ export const MockReportService = {
                   width: 120,
                   align: 'right',
                   format: FORMAT_VALUES_PRICE,
-
                   type: 'number',
                 },
               ],
@@ -308,7 +307,7 @@ export const MockReportService = {
                       editable: false,
                       width: 120,
                       align: 'right',
-                      format: FORMAT_VALUES_3_DECIMAL,
+                      format: FORMAT_VALUES_NORMS,
 
                       type: 'number',
                     },
