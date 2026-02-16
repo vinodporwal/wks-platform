bea2f1f7cb0677c504ddb82147749928e151657b
Ankit <ankitofficial95@gmail.com>
2026-02-16 12:56:12 +0530

    backend changes

--- apps/java/libraries/case-engine/src/main/java/com/wks/caseengine/service/MaintenanceCalculatedDataService.java
+++ apps/java/libraries/case-engine/src/main/java/com/wks/caseengine/service/MaintenanceCalculatedDataService.java
@@ -15,6 +15,7 @@ public interface MaintenanceCalculatedDataService {
 	
 	public List<MaintenanceDetailsDTO> getMaintenanceCalculatedData(String plantId, String year);
 	public AOPMessageVM getMaintenanceReportURLs(String plantId, String year,String type);
+	public AOPMessageVM getOtherPlants( String plantId,  String year);
 	public AOPMessageVM getMaintenanceDataForCracker( String plantId,  String year);
 	public AOPMessageVM updateMaintenanceDataForCracker( String plantId,  String year, List<Map<String, Object>> payloadList);
 	public AOPMessageVM getBudgetMaintenance( String plantId,  String year,String budgetCategory);
