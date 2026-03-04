0ff81f9f6845dd8bbf497e95475e3e1f0fd80464
Shivanand Raut <shivanandraut007@gmail.com>
2026-03-04 22:13:54 +0530

    Production target Export Import code

--- apps/react/case-portal/src/components/kendo-data-tables/ProductionVoluemData.js
+++ apps/react/case-portal/src/components/kendo-data-tables/ProductionVoluemData.js
@@ -960,7 +960,7 @@ const ProductionvolumeData = ({ permissions }) => {
       showTitleNameBusiness:
         VERTICAL_NAME !== 'cracker' && VERTICAL_NAME !== 'vcm' ? true : false,
 
-      downloadExcelBtnFromUI: IS_PE_PP ? false : true,
+      downloadExcelBtnFromUI: IS_PE_PP || IS_PET ? false : true,
       ExcelName: `${EXCEL_EXPORT_TITLE}_Max Achieved Capacity`,
     },
     isOldYear,
@@ -983,9 +983,9 @@ const ProductionvolumeData = ({ permissions }) => {
       units: ['TPH', 'TPD'],
 
       // downloadExcelBtn: permissions?.hideDownloadExcel ? false : true,
-      downloadExcelBtnFromUI: IS_PE_PP ? false : true,
-      downloadExcelBtn: IS_PE_PP ? true : false,
-      uploadExcelBtn: IS_PE_PP ? true : false,
+      downloadExcelBtnFromUI: IS_PE_PP || IS_PET ? false : true,
+      downloadExcelBtn: IS_PE_PP || IS_PET ? true : false,
+      uploadExcelBtn: IS_PE_PP || IS_PET ? true : false,
       ExcelName: `${EXCEL_EXPORT_TITLE}_Design Capacity`,
 
       showTitleAndInformation:
@@ -1028,8 +1028,8 @@ const ProductionvolumeData = ({ permissions }) => {
         Object.keys(calculationObject || {}).length > 0
           ? true
           : false,
-      downloadExcelBtn: IS_PE_PP ? false : true,
-      uploadExcelBtn: IS_PE_PP ? false : true,
+      downloadExcelBtn: IS_PE_PP || IS_PET ? false : true,
+      uploadExcelBtn: IS_PE_PP || IS_PET ? false : true,
 
       showTitleAndInformation:
         VERTICAL_NAME == 'cracker' || VERTICAL_NAME == 'vcm' ? true : false,
@@ -1096,7 +1096,7 @@ const ProductionvolumeData = ({ permissions }) => {
     })
 
     try {
-      if (IS_PE_PP) {
+      if (IS_PE_PP || IS_PET) {
         await ProductionVolumeDataApiService.getProductionVolExcelCommon(
           keycloak,
           PLANT_ID,
