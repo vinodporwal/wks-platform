f31f7038c597aa9a24395820b78dc10afa7a979a
nirajgupta9218 <niraj.gupta9218@gmail.com>
2026-03-17 20:21:47 +0530

    C:/Program Files/Git/monthly production

--- apps/java/libraries/case-engine/src/main/java/com/wks/caseengine/service/AOPServiceImpl.java
+++ apps/java/libraries/case-engine/src/main/java/com/wks/caseengine/service/AOPServiceImpl.java
@@ -167,7 +167,7 @@ public class AOPServiceImpl implements AOPService {
 			        aopDTO.setFeb(safeParseDouble(row[16]));
 			        aopDTO.setMarch(safeParseDouble(row[17]));
 			        aopDTO.setAvgTPH(safeParseDouble(row[18]));
-			        aopDTO.setRemark(row[19] != null ? row[19].toString() : null);
+			        aopDTO.setRemark(row[19] != null ? row[19].toString() : "");
 				aopDTO.setDisplayOrder(row[20] != null ? Integer.valueOf(row[20].toString()) : null);
 				aopDTO.setIsEditable(row[21] != null ? Boolean.valueOf(row[21].toString()) : null);
 				aopDTO.setIsVisible(row[22] != null ? Boolean.valueOf(row[22].toString()) : null);
