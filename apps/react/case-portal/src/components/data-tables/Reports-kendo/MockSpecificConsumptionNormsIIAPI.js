2495dfb9dd78b76f763ad943d18e65bc879b4348
Ankit <ankitofficial95@gmail.com>
2026-03-27 20:36:43 +0530

    case added for Utility in Specific Consumption Report

--- apps/react/case-portal/src/components/data-tables/Reports-kendo/MockSpecificConsumptionNormsIIAPI.js
+++ apps/react/case-portal/src/components/data-tables/Reports-kendo/MockSpecificConsumptionNormsIIAPI.js
@@ -977,6 +977,165 @@ export const MockSpecificConsumptionNormsIIAPI = {
             },
           ],
         }
+      case 'Utilities':
+        return {
+          columns: [
+            {
+              field: 'sno',
+              title: 'S.no',
+              widthT: 58,
+              editable: false,
+              align: 'right',
+              format: '{0:0}',
+            },
+            {
+              field: 'id',
+              title: 'Id',
+              editable: false,
+              hidden: true,
+            },
+            {
+              field: 'material',
+              title: 'Raw material',
+              editable: false,
+            },
+            {
+              field: 'price',
+              title: 'Price (Rs/MT)',
+              editable: false,
+              align: 'right',
+              format: valueFormat,
+              type: 'number',
+            },
+            { field: 'uom', title: 'Unit', widthT: 60, editable: false },
+            // { field: 'design', title: 'Design', editable: false },
+            {
+              title: 'Design',
+              children: [
+                {
+                  field: 'design',
+                  title: 'Norms',
+                  editable: false,
+                },
+                {
+                  field: 'designRsMt',
+                  title: 'Rs/MT',
+                  editable: false,
+                },
+              ],
+            },
+            // {
+            //   field: 'bestAchievedSinceLastFourYears',
+            //   title: 'Best Achieved (last 4 years)',
+            //   format: valueFormat,
+            //   type: 'number',
+            //   editable: false,
+            // },
+            {
+              title: 'Best Achieved Since Commissioning',
+              children: [
+                {
+                  field: 'bestAchivedActual',
+                  title: 'Norms',
+                  editable: false,
+                },
+                {
+                  field: 'bestAchivedActualRsMT',
+                  title: 'Rs/MT',
+                  editable: false,
+                },
+              ],
+            },
+            // {
+            //   field: 'globalBenchmark',
+            //   title: 'Global Benchmark',
+            //   editable: false,
+            // },
+            {
+              title: 'Global Benchmark',
+              children: [
+                {
+                  field: 'globalBenchmark',
+                  title: 'Norms',
+                  editable: false,
+                },
+                {
+                  field: 'globalBenchmarkRsMT',
+                  title: 'Rs/MT',
+                  editable: false,
+                },
+              ],
+            },
+
+            {
+              title: `Budget ${prevFY1}`,
+              children: [
+                {
+                  field: 'budgetPrevYear',
+                  title: 'Norms',
+                  editable: false,
+                  align: 'right',
+                  format: valueFormat,
+                  type: 'number',
+                },
+                {
+                  field: 'budgetPrevYearRsMT',
+                  title: 'Rs/MT',
+                  editable: false,
+                  align: 'right',
+                  format: valueFormat,
+                  type: 'number',
+                },
+              ],
+            },
+            {
+              title: `Actual ${prevFY1}`,
+              children: [
+                {
+                  field: 'actualPrevYear',
+                  title: 'Norms',
+                  editable: false,
+                  align: 'right',
+                  format: valueFormat,
+                  type: 'number',
+                },
+                {
+                  field: 'actualPrevYearRsMT',
+                  title: 'Rs/MT',
+                  editable: false,
+                  align: 'right',
+                  format: valueFormat,
+                  type: 'number',
+                },
+              ],
+            },
+            {
+              title: `Proposed ${currFY}`,
+              children: [
+                {
+                  field: 'proposedBudget',
+                  title: 'Norms',
+                  editable: false,
+                  format: valueFormat,
+                  type: 'number',
+                },
+                {
+                  field: 'proposedBudgetRsMT',
+                  title: 'Rs/MT',
+                  editable: false,
+                  format: valueFormat,
+                  type: 'number',
+                },
+              ],
+            },
+            {
+              field: 'remarks',
+              title: 'Remarks',
+              editable: true,
+              fixedWidth: 110,
+            },
+          ],
+        }
 
       default:
         return { columns: [] }
