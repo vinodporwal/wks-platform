faab3525aa1dca2acbbb34a454b9e3129498ec7f
nirajgupta9218 <niraj.gupta9218@gmail.com>
2026-03-30 15:33:09 +0530

    HHIR shutdown export and import

---
+++ apps/java/libraries/case-engine/src/main/java/com/wks/caseengine/service/ShutdownSlowdownExportImportService.java
@@ -0,0 +1,15 @@
+package com.wks.caseengine.service;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.web.multipart.MultipartFile;
+import com.wks.caseengine.dto.ShutDownPlanDTO;
+import com.wks.caseengine.message.vm.AOPMessageVM;
+
+public interface ShutdownSlowdownExportImportService {
+	
+	public byte[] exportShutdown(String year, String plantFKId,boolean isAfterSave,List<ShutDownPlanDTO> dtoList);
+	public AOPMessageVM importShutdown(String year,UUID plantId,MultipartFile file);
+	
+}
