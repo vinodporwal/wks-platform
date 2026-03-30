b46e42e54694c58e9c5477b27c6663833eb76688
Kiran Kadam <kadamkiran3179@gmail.com>
2026-03-30 17:47:00 +0530

    steady state norms import and export api integration

--- apps/react/case-portal/src/components/aop-phase-two/crude/steady-state-consumption/index.js
+++ apps/react/case-portal/src/components/aop-phase-two/crude/steady-state-consumption/index.js
@@ -410,12 +410,13 @@ const SteadyStateConsumption = () => {
           AOP_YEAR,
           file,
         )
-      setRows(importedData)
-      setOriginalRows(importedData)
+      setRows([])
+      setOriginalRows([])
       setSnackbarData({
         message: 'Data imported successfully!',
         severity: 'success',
       })
+      await fetchData()
     } catch (error) {
       console.error('Error importing steady state consumption data:', error)
       setSnackbarData({
@@ -477,7 +478,7 @@ const SteadyStateConsumption = () => {
         setCurrentRowId={() => {}}
         saveChanges={saveChanges}
         handleExport={handleExport}
-        handleImport={handleImport}
+        handleExcelUpload={handleImport}
         handleCalculate={handleCalculate}
         snackbarData={snackbarData}
         snackbarOpen={snackbarOpen}
