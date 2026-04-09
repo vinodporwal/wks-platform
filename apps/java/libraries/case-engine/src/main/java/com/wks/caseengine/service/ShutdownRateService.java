8ee34c39204310da6a78e7e3b93dc20d3e5b480a
nirajgupta9218 <niraj.gupta9218@gmail.com>
2026-04-09 16:00:29 +0530

    Shutdown rate dropdown

--- apps/java/libraries/case-engine/src/main/java/com/wks/caseengine/service/ShutdownRateService.java
+++ apps/java/libraries/case-engine/src/main/java/com/wks/caseengine/service/ShutdownRateService.java
@@ -1,9 +1,12 @@
 package com.wks.caseengine.service;
 
 import com.wks.caseengine.message.vm.AOPMessageVM;
+import java.util.List;
 
 public interface ShutdownRateService {
 	
 	public AOPMessageVM getShutdownRate(String plantId, String aopYear);
+	
+	public List<com.wks.caseengine.dto.ShutdownRateDropdownDTO> getShutdownRateDropdown( String plantId);
 	
 }
