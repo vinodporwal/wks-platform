e48897143b298446d264d6cb1220f61309a882ff
shrikantp2143 <shrikantp2143@gmail.com>
2026-05-26 18:38:16 +0530

    test

--- apps/java/services/case-engine-rest-api/src/main/java/com/wks/caseengine/rest/cpp/JMDAssetsController.java
+++ apps/java/services/case-engine-rest-api/src/main/java/com/wks/caseengine/rest/cpp/JMDAssetsController.java
@@ -1,9 +1,12 @@
 package com.wks.caseengine.rest.cpp;
 
 import com.wks.caseengine.cpp.service.JMDAssetsService;
+import com.wks.caseengine.dto.JMDOperationalHoursRequestDTO;
 import com.wks.caseengine.message.vm.AOPMessageVM;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,5 +27,14 @@ public class JMDAssetsController {
             @RequestParam String financialYear) {
 
         return jmdAssetsService.getOperationalHoursForPlants(plantIds, financialYear);
+    }
+
+    @PostMapping("/jmd/assets/operational-hours")
+    public AOPMessageVM saveOperationalHours(
+            @RequestParam List<UUID> plantIds,
+            @RequestParam String financialYear,
+            @RequestBody JMDOperationalHoursRequestDTO payload) {
+
+        return jmdAssetsService.saveOperationalHours(plantIds, financialYear, payload);
     }
 }
