c8eacddcfc27ee5ba17b748caf65328107c398de
shrikantp2143 <shrikantp2143@gmail.com>
2026-05-26 13:19:50 +0530

    operational-hrs

--- apps/java/services/case-engine-rest-api/src/main/java/com/wks/caseengine/rest/cpp/PowerGenerationController.java
+++ apps/java/services/case-engine-rest-api/src/main/java/com/wks/caseengine/rest/cpp/PowerGenerationController.java
@@ -1,11 +1,15 @@
 package com.wks.caseengine.rest.cpp;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.wks.caseengine.dto.CPPAssetOperationalHoursResponseDto;
 import com.wks.caseengine.dto.MasterAssetOperationalResponseDTO;
 import com.wks.caseengine.message.vm.AOPMessageVM;
 import com.wks.caseengine.cpp.service.PowerGenerationService;
@@ -26,6 +31,9 @@ public class PowerGenerationController {
     @Autowired
     private PowerGenerationService powerGenerationService;
 
+    @Autowired
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
 
     @GetMapping("/assets/operational-hours/{plantId}/{financialYear}")
     public ResponseEntity<MasterAssetOperationalResponseDTO> getAssetOperationalHours(
@@ -36,6 +44,90 @@ public class PowerGenerationController {
                 powerGenerationService.getAssetOperationalHours(plantId, financialYear);
 
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/assets/operational-hours")
+    public ResponseEntity<List<CPPAssetOperationalHoursResponseDto>> getCppAssetOperationalHoursForPlants(
+            @RequestParam List<UUID> plantIds,
+            @RequestParam String financialYear) {
+
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("plantIds", plantIds);
+        params.addValue("financialYear", financialYear);
+
+        String sql = """
+            SELECT
+                h.Id AS id,
+                h.Asset_FK_Id AS assetFkId,
+                h.utility_distributed AS utilityDistributed,
+                h.distributed_sap_code AS distributedSapCode,
+                h.utility_generated AS utilityGenerated,
+                h.generated_utility_code AS generatedUtilityCode,
+                h.Apr AS apr,
+                h.May AS may,
+                h.Jun AS jun,
+                h.Jul AS jul,
+                h.Aug AS aug,
+                h.Sep AS sep,
+                h.Oct AS oct,
+                h.Nov AS nov,
+                h.[Dec] AS dec,
+                h.Jan AS jan,
+                h.Feb AS feb,
+                h.Mar AS mar,
+                CAST(h.AOPYear AS VARCHAR(10)) AS aopYear,
+                h.Remarks AS remarks,
+                h.Site_FK_Id AS siteFkId,
+                h.Vertical_FK_ID AS verticalFkId,
+                h.Plant_FK_Id AS plantFkId,
+                h.CreatedDate AS createdDate,
+                h.ModifiedDate AS modifiedDate,
+                pga.AssetName AS assetName,
+                pga.AssetType AS assetType,
+                pl.DisplayName AS plantName
+            FROM [RIL.AOP].[dbo].[CPPAssetOperationalHours] h WITH(NOLOCK)
+            LEFT JOIN PowerGenerationAssets pga WITH(NOLOCK)
+                ON pga.AssetId = h.Asset_FK_Id
+            LEFT JOIN Plants pl WITH(NOLOCK)
+                ON pl.Id = h.Plant_FK_Id
+            WHERE h.Plant_FK_Id IN (:plantIds)
+              AND h.AOPYear = :financialYear
+        """;
+
+        List<CPPAssetOperationalHoursResponseDto> result = namedParameterJdbcTemplate.query(sql, params, (rs, rowNum) -> {
+            CPPAssetOperationalHoursResponseDto dto = new CPPAssetOperationalHoursResponseDto();
+            dto.setId(rs.getObject("id", UUID.class));
+            dto.setAssetFkId(rs.getObject("assetFkId", UUID.class));
+            dto.setUtilityDistributed(rs.getString("utilityDistributed"));
+            dto.setDistributedSapCode(rs.getString("distributedSapCode"));
+            dto.setUtilityGenerated(rs.getString("utilityGenerated"));
+            dto.setGeneratedUtilityCode(rs.getString("generatedUtilityCode"));
+            dto.setApr(rs.getObject("apr") != null ? rs.getDouble("apr") : null);
+            dto.setMay(rs.getObject("may") != null ? rs.getDouble("may") : null);
+            dto.setJun(rs.getObject("jun") != null ? rs.getDouble("jun") : null);
+            dto.setJul(rs.getObject("jul") != null ? rs.getDouble("jul") : null);
+            dto.setAug(rs.getObject("aug") != null ? rs.getDouble("aug") : null);
+            dto.setSep(rs.getObject("sep") != null ? rs.getDouble("sep") : null);
+            dto.setOct(rs.getObject("oct") != null ? rs.getDouble("oct") : null);
+            dto.setNov(rs.getObject("nov") != null ? rs.getDouble("nov") : null);
+            dto.setDec(rs.getObject("dec") != null ? rs.getDouble("dec") : null);
+            dto.setJan(rs.getObject("jan") != null ? rs.getDouble("jan") : null);
+            dto.setFeb(rs.getObject("feb") != null ? rs.getDouble("feb") : null);
+            dto.setMar(rs.getObject("mar") != null ? rs.getDouble("mar") : null);
+            dto.setAopYear(rs.getString("aopYear"));
+            dto.setRemarks(rs.getString("remarks"));
+            dto.setSiteFkId(rs.getObject("siteFkId", UUID.class));
+            dto.setVerticalFkId(rs.getObject("verticalFkId", UUID.class));
+            dto.setPlantFkId(rs.getObject("plantFkId", UUID.class));
+            dto.setCreatedDate(rs.getObject("createdDate", LocalDateTime.class));
+            dto.setModifiedDate(rs.getObject("modifiedDate", LocalDateTime.class));
+            dto.setAssetName(rs.getString("assetName"));
+            dto.setPlantName(rs.getString("plantName"));
+            dto.setAssetType(rs.getString("assetType"));
+            return dto;
+        });
+
+        return ResponseEntity.ok(result);
     }
 
     @PostMapping("/assets/operational-hours/{plantId}/{financialYear}")
