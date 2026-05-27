d6ccf883619d354b75e91123f922379a34885c9f
Neha Chaudhari <neha.chaudhari2@honeywell.com>
2026-05-27 15:04:14 +0530

    To Display Correct caseAssignedTo in Case List

--- apps/react/case-portal/src/views/caseList/caseList.js
+++ apps/react/case-portal/src/views/caseList/caseList.js
@@ -668,7 +668,7 @@ export const CaseList = ({ status, caseDefId: caseDefIdProp }) => {
               ? JSON.parse(containerValue)
               : {}
 
-            return parsedContainer.caseAssignedTo || parsedContainer.caseAssignedTo || ''
+            return parsedContainer.caseAssignedTo.label || parsedContainer.caseAssignedTo.email[0] || ''
           } catch (error) {
             console.error('Error parsing CaseAssignedTo:', error)
             return ''
