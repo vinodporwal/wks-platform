c3ee2e23ecb5a4f28eed37e6e56aceedd4b219f5
Nitesh Patidar <nitesh.patidar@mindnerves.com>
2026-06-11 14:37:52 +0530

    feat: implement production norms configuration module with data services and routing

--- apps/react/case-portal/src/routes/MainRoutes.js
+++ apps/react/case-portal/src/routes/MainRoutes.js
@@ -149,7 +149,11 @@ import SteadyStateConsumptionMerox from 'components/aop-phase-two/merox/steady-s
 import OverallAopConsumptionMerox from 'components/aop-phase-two/merox/overall-aop-consumption'
 import ProductionNormsBasisMerox from 'components/aop-phase-two/merox/production-norms-basis'
 // Vertical MEROX Ended
-
+// PCG
+import SteadyStateConsumptionPCG from 'components/aop-phase-two/pcg/steady-state-consumption'
+import OverallAopConsumptionPCG from 'components/aop-phase-two/pcg/overall-aop-consumption'
+import ProductionNormsBasisPCG from 'components/aop-phase-two/pcg/production-norms-basis'
+// PCG Ended
 const ManagamentDefault = Loadable(lazy(() => import('../views/management')))
 const DashboardDefault = Loadable(lazy(() => import('../views/dashboard')))
 
@@ -924,6 +928,33 @@ export const MainRoutes = (
             ),
           },
           //Vertical MEROX Ended
+
+          //Vertical PCG Started
+          {
+            path: 'production-norms-basis-pcg',
+            element: (
+              <PrivateRoute routeId='production-norms-basis-pcg'>
+                <ProductionNormsBasisPCG />
+              </PrivateRoute>
+            ),
+          },
+          {
+            path: 'steady-state-consumption-pcg',
+            element: (
+              <PrivateRoute routeId='steady-state-consumption-pcg'>
+                <SteadyStateConsumptionPCG />
+              </PrivateRoute>
+            ),
+          },
+          {
+            path: 'overall-aop-consumption-pcg',
+            element: (
+              <PrivateRoute routeId='overall-aop-consumption-pcg'>
+                <OverallAopConsumptionPCG />
+              </PrivateRoute>
+            ),
+          },
+          //Vertical PCG Ended
         ],
       },
 
