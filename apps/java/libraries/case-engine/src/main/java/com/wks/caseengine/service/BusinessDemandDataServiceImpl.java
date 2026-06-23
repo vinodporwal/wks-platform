546c6f58c725588828b915840522b2125b1dd813
karan.kapoor <karan.kapoor@vetology.net>
2026-06-23 11:14:08 +0530

    business demand get api fixes

--- apps/java/libraries/case-engine/src/main/java/com/wks/caseengine/service/BusinessDemandDataServiceImpl.java
+++ apps/java/libraries/case-engine/src/main/java/com/wks/caseengine/service/BusinessDemandDataServiceImpl.java
@@ -123,9 +123,13 @@ public class BusinessDemandDataServiceImpl implements BusinessDemandDataService 
 			if(verticalName.equalsIgnoreCase("CRACKER") || verticalName.equalsIgnoreCase("PE") || verticalName.equalsIgnoreCase("PP") || verticalName.equalsIgnoreCase("PET") || verticalName.equalsIgnoreCase("Elastomer")  || verticalName.equalsIgnoreCase("Chemical") || vertical.getName().equalsIgnoreCase("Staple") || vertical.getName().equalsIgnoreCase("Filament")) {
 				String procedureName=verticalName+"_GetBusinessDemand";
 				obj=findByYearAndPlantId(year,UUID.fromString(plantId),procedureName);
+				Map<String, Object> map = new HashMap<>();
+
+				map.put("businessDemandDataDTOList", obj);
+				map.put("aopCalculation", null);
 				AOPMessageVM aopMessageVM = new AOPMessageVM();
 				aopMessageVM.setCode(200);
-				aopMessageVM.setData(getBusinessDemand(obj));
+				aopMessageVM.setData(map);
 				aopMessageVM.setMessage("Data fetched successfully");
 				return aopMessageVM;
 			}else {
