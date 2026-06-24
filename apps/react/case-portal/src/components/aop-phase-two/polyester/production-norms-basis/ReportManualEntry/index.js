44d9cb37c2e6a5129961d1ae8db6a2e7b9ecefcc
Nitesh Patidar <nitesh.patidar@mindnerves.com>
2026-06-24 16:20:00 +0530

    seprate screen route for PET vertical plant wise

--- apps/react/case-portal/src/components/aop-phase-two/polyester/production-norms-basis/ReportManualEntry/index.js
+++ apps/react/case-portal/src/components/aop-phase-two/polyester/production-norms-basis/ReportManualEntry/index.js
@@ -340,8 +340,6 @@ const ManualEntry = ({ startDate, endDate }) => {
     setCurrentRowId(row.id)
     setRemarkDialogOpen(true)
   }
-  console.log('originalRows', rows)
-  console.log('modifiedCells', modifiedCells)
   return (
     <Box>
       <LoaderBackdrop open={!!loading} />
