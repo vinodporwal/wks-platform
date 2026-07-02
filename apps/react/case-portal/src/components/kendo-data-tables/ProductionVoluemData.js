a3dc707ead051e6b9efc583028c03f0e69eba920
Shivanand Raut <shivanandraut007@gmail.com>
2026-07-02 19:27:47 +0530

    Production Target Import code

--- apps/react/case-portal/src/components/kendo-data-tables/ProductionVoluemData.js
+++ apps/react/case-portal/src/components/kendo-data-tables/ProductionVoluemData.js
@@ -1690,6 +1690,7 @@ const ProductionvolumeData = ({
         // setLoading(false)
 
         fetchData()
+        fetchDesignCapacityData()
         if (IS_CRACKER_DMD || IS_CRACKER_C2) {
           fetchDesignCapacityData(unitDesignCapacity)
         }
