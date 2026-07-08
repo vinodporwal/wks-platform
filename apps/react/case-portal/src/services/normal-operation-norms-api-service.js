200ae0ace1d2a46d99ea172707e7456ecd43a913
Shivanand Raut <shivanandraut007@gmail.com>
2026-07-08 21:16:49 +0530

    Cat chem For PVC DMD

--- apps/react/case-portal/src/services/normal-operation-norms-api-service.js
+++ apps/react/case-portal/src/services/normal-operation-norms-api-service.js
@@ -35,6 +35,8 @@ export const NormalOperationNormsApiService = {
   shutdownNormsExportAllGarde,
   getNormalOpsNormsExcelChemicalDmd,
   saveNormalOpsNormsExcelChemicalDmd,
+  getCatChemCalculationData,
+  saveCatChemCalculationData,
 }
 
 async function BestAchivedColorCodes(keycloak, plantId, year, mode) {
@@ -913,6 +915,36 @@ async function saveNormalOpsNormsExcelChemicalDmd(
       headers,
       body: formData,
     })
+    return json(keycloak, resp)
+  } catch (e) {
+    console.log(e)
+    return await Promise.reject(e)
+  }
+}
+async function getCatChemCalculationData(keycloak, PLANT_ID, AOP_YEAR) {
+  const url = `${Config.CaseEngineUrl}/task/catalyst-chemicals-calculation-data?plantId=${PLANT_ID}&year=${AOP_YEAR}`
+  const headers = {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${keycloak.token}`,
+  }
+  try {
+    const resp = await fetch(url, { method: 'GET', headers })
+    return json(keycloak, resp)
+  } catch (e) {
+    console.log(e)
+    return await Promise.reject(e)
+  }
+}
+async function saveCatChemCalculationData(keycloak, PLANT_ID, AOP_YEAR, payload) {
+  const url = `${Config.CaseEngineUrl}/task/save-catalyst-chemicals-calculation-data?plantId=${PLANT_ID}&year=${AOP_YEAR}`
+  const headers = {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${keycloak.token}`,
+  }
+  try {
+    const resp = await fetch(url, { method: 'POST', headers, body: JSON.stringify(payload) })
     return json(keycloak, resp)
   } catch (e) {
     console.log(e)
