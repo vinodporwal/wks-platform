d7a8486be3dcb70dbe5af93b04f49057fe2ebf62
Kiran Kadam <kadamkiran3179@gmail.com>
2026-07-27 18:14:23 +0530

    code cleanup

--- apps/react/case-portal/src/components/aop-phase-two/cpp/jmd/qty-cost-report/index.js
+++ apps/react/case-portal/src/components/aop-phase-two/cpp/jmd/qty-cost-report/index.js
@@ -1,42 +1,13 @@
 import React from 'react'
-import { Box, Stack } from '@mui/material'
-import { useSelector } from 'react-redux'
-import LoaderBackdrop from 'components/Utilities/LoaderBackdrop'
 import NormsQtyCostReportMonthly from './NormsQtyCostReportMonthly'
 import NormsQtyCostReportAnnual from './NormsQtyCostReportAnnual'
 
 const QtyCostReportJMD = () => {
-  const dataGridStore = useSelector((state) => state.dataGridStore)
-  const { siteObject, verticalObject } = dataGridStore
-
-  const lowerVertName = verticalObject?.name?.toLowerCase()
-  const lowerSiteName = siteObject?.name?.toLowerCase()
-  const IS_CPP = lowerVertName === 'cpp'
-
-  const renderBySite = () => {
-    switch (lowerSiteName) {
-      case 'nmd':
-      default:
-        return (
-          <Box>
-            <Stack>
-              <NormsQtyCostReportMonthly />
-            </Stack>
-            <Stack>
-              <NormsQtyCostReportAnnual />
-            </Stack>
-          </Box>
-        )
-    }
-  }
-
-  if (!IS_CPP) return null
-
   return (
-    <Box>
-      <LoaderBackdrop open={false} />
-      {renderBySite()}
-    </Box>
+    <div>
+      <NormsQtyCostReportMonthly />
+      <NormsQtyCostReportAnnual />
+    </div>
   )
 }
 
