5ad7db5c6f00751a824617b69bd9942c3b13a5f8
Kiran Kadam <kadamkiran3179@gmail.com>
2026-08-03 11:22:19 +0530

    cpp output report heat rate

--- apps/react/case-portal/src/i18n/en_us.js
+++ apps/react/case-portal/src/i18n/en_us.js
@@ -44,7 +44,8 @@ const defs = {
     shutdownNorms: 'Shutdown Consumption (Quantity)',
     slowdownNorms: 'Slowdown Consumption (Norm/Quantity)',
     normalOpNorms: 'Steady State Consumption (Norm/Quantity)',
-    normalOpNormsGradeWise: 'Grade Wise Steady State Consumption (Norm/Quantity)',
+    normalOpNormsGradeWise:
+      'Grade Wise Steady State Consumption (Norm/Quantity)',
     slowdownPlan: 'Slowdown Activities',
     taPlan: 'Turnaround Activities',
     feedStock: 'Feed Stock Availability',
@@ -63,6 +64,7 @@ const defs = {
     norms_qty_cost_report: 'Norms Quantity Cost Report',
     utility_rate: 'Utility Rate',
     inputs: 'Inputs',
+    outputs: 'Outputs',
     summary: 'Summary',
     senderReceiverMapping: 'Sender Receiver Mapping',
     importPower: 'Import Power',
@@ -145,7 +147,7 @@ const defs = {
     vcmAvailability: 'VCM Availability',
     otherDocumentUpload: 'Other Document Upload',
     productionScheduling: 'Production Scheduling',
-    configurationOtherCost: 'Other Cost'
+    configurationOtherCost: 'Other Cost',
   },
   pages: {
     dashboard: {
@@ -290,7 +292,8 @@ const defs = {
         shutdownPlan: 'Shutdown Activities',
         shutdownNorms: 'Shutdown Consumption (Quantity)',
         normalOpNorms: 'Steady State Consumption (Norm/Quantity)',
-        normalOpNormsGradeWise: 'Grade Wise Steady State Consumption (Norm/Quantity)',
+        normalOpNormsGradeWise:
+          'Grade Wise Steady State Consumption (Norm/Quantity)',
         slowdownPlan: 'Slowdown Activities',
         taPlan: 'Turnaround Activities',
         productionNormsOutput: 'Production Norms (Output)',
