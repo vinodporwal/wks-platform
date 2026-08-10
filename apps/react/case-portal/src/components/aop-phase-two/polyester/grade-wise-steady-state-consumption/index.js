299706dfb18267b3d40dba5efd65ea8480883f8e
Nitesh Patidar <nitesh.patidar@mindnerves.com>
2026-08-10 13:07:34 +0530

    feat: implement new consumption and production norm components for AOP phase two

--- apps/react/case-portal/src/components/aop-phase-two/polyester/grade-wise-steady-state-consumption/index.js
+++ apps/react/case-portal/src/components/aop-phase-two/polyester/grade-wise-steady-state-consumption/index.js
@@ -99,7 +99,7 @@ const GradeWiseSteadyStateConsumption = () => {
     },
     {
       field: 'sapCode',
-      title: 'Sap Code',
+      title: 'SAP Code',
       minWidth: 120,
       type: 'text',
       editable: false,
