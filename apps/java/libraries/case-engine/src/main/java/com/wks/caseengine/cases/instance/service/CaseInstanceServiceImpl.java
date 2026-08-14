453af56440845a269d123d4a00c46c43a4c5152a
Pranav Raut <pranavraut7028@gmail.com>
2026-08-14 18:32:31 +0530

    fix-overdue case email scheduled

--- apps/java/libraries/case-engine/src/main/java/com/wks/caseengine/cases/instance/service/CaseInstanceServiceImpl.java
+++ apps/java/libraries/case-engine/src/main/java/com/wks/caseengine/cases/instance/service/CaseInstanceServiceImpl.java
@@ -151,7 +151,7 @@ public class CaseInstanceServiceImpl implements CaseInstanceService {
 	}
 
     @Override
-	@Scheduled(cron = "0 0 20 * * *", zone = "Asia/Kolkata")
+	@Scheduled(cron = "0 40 19 * * *", zone = "Asia/Kolkata")
 	public void findCasesWithDueDateGreaterThanNow() {
 		
 	commandContext.getSecurityContextTenantHolder().setTenantId(dbTenant);
