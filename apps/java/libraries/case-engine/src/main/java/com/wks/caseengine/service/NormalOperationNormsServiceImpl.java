4007e4b03f5f7cf77dad64fb49f39b5625cf1715
nirajgupta9218 <niraj.gupta9218@gmail.com>
2026-08-18 18:01:22 +0530

    Manual entry

--- apps/java/libraries/case-engine/src/main/java/com/wks/caseengine/service/NormalOperationNormsServiceImpl.java
+++ apps/java/libraries/case-engine/src/main/java/com/wks/caseengine/service/NormalOperationNormsServiceImpl.java
@@ -206,6 +206,9 @@ public class NormalOperationNormsServiceImpl implements NormalOperationNormsServ
 					mCUNormsValueDTO.setNormParameterTypeDisplayName(row[25] != null ? row[25].toString() : null);
 					mCUNormsValueDTO.setUOM(row[26] != null ? row[26].toString() : null);
 					mCUNormsValueDTO.setIsEditable(row[27] != null ? Boolean.valueOf(row[27].toString()) : null);
+					if(vertical.getName().equalsIgnoreCase("Filament") && mCUNormsValueDTO.getNormParameterTypeName().equalsIgnoreCase("Manual Entry")) {
+						mCUNormsValueDTO.setIsEditable(true);
+					}
 					mCUNormsValueDTO.setProductName(row[28] != null ? row[28].toString() : null);
 					if(vertical.getName().equalsIgnoreCase("STAPLE") || vertical.getName().equalsIgnoreCase("Filament")){
 					mCUNormsValueDTO.setSapCode(row[29] != null ? row[29].toString() : "");
