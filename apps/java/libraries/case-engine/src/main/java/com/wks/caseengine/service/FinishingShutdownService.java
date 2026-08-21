8bccb1768c375bdd065a0ba1014c56e17e8977ed
karan.kapoor <karan.kapoor@vetology.net>
2026-08-21 12:18:00 +0530

    import and export apis for finishing shutdown config

--- apps/java/libraries/case-engine/src/main/java/com/wks/caseengine/service/FinishingShutdownService.java
+++ apps/java/libraries/case-engine/src/main/java/com/wks/caseengine/service/FinishingShutdownService.java
@@ -2,14 +2,17 @@ package com.wks.caseengine.service;
 
 import java.util.List;
 
-import com.wks.caseengine.dto.FinishingShutdownConfigDTO;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.wks.caseengine.dto.FinishingShutdownConfigDTO;
 import com.wks.caseengine.message.vm.AOPMessageVM;
 
 public interface FinishingShutdownService {
 
-	
-	public AOPMessageVM getFinishingShutdown(String plantId,String year);
-	public AOPMessageVM saveFinishingShutdown( String year, String plantFKId, List<FinishingShutdownConfigDTO> finishingShutdownConfigDTOs);
+	public AOPMessageVM getFinishingShutdown(String plantId, String year);
+	public List<FinishingShutdownConfigDTO> saveFinishingShutdown(String year, String plantFKId, List<FinishingShutdownConfigDTO> finishingShutdownConfigDTOs);
 	public AOPMessageVM deleteFinishingShutdown(String id);
+
+	byte[] createFinishingShutdownExcel(String plantId, String year, boolean isAfterSave, List<FinishingShutdownConfigDTO> dtoList);
+	AOPMessageVM importFinishingShutdownExcel(String year, String plantId, MultipartFile file);
 }
