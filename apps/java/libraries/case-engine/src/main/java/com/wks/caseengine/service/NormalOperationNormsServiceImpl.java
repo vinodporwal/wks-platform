f79a6c156e6caa6d3a2eff91d79f3f34832a02e8
nirajgupta9218 <niraj.gupta9218@gmail.com>
2026-09-08 09:04:00 +0530

    SAP Code for FCC

--- apps/java/libraries/case-engine/src/main/java/com/wks/caseengine/service/NormalOperationNormsServiceImpl.java
+++ apps/java/libraries/case-engine/src/main/java/com/wks/caseengine/service/NormalOperationNormsServiceImpl.java
@@ -223,15 +223,13 @@ public class NormalOperationNormsServiceImpl implements NormalOperationNormsServ
 					if(vertical.getName().equalsIgnoreCase("STAPLE") || vertical.getName().equalsIgnoreCase("Filament")){
 					mCUNormsValueDTO.setSapCode(row[29] != null ? row[29].toString() : "");
 					}
-					if(vertical.getName().equalsIgnoreCase("CRUDE") || vertical.getName().equalsIgnoreCase("Coker") || vertical.getName().equalsIgnoreCase("MEROX") || vertical.getName().equalsIgnoreCase("VGOHT") || vertical.getName().equalsIgnoreCase("PCG")) {
+					if(vertical.getName().equalsIgnoreCase("CRUDE") || vertical.getName().equalsIgnoreCase("Coker") || vertical.getName().equalsIgnoreCase("MEROX") || vertical.getName().equalsIgnoreCase("VGOHT") || vertical.getName().equalsIgnoreCase("PCG") || vertical.getName().equalsIgnoreCase("FCC")) {
 						mCUNormsValueDTO.setSapCode(row[29] != null ? row[29].toString() : "");
 					}
 					if (vertical.getName().equalsIgnoreCase("VCM") || vertical.getName().equalsIgnoreCase("PTA") || vertical.getName().equalsIgnoreCase("Chemical")) {
-						mCUNormsValueDTO.setWtAverage(row[29] != null ? Double.parseDouble(row[29].toString()) : null);
-						
+						mCUNormsValueDTO.setWtAverage(row[29] != null ? Double.parseDouble(row[29].toString()) : null);	
 					}
-					if (chemical || ptaPmdPia) {
-						
+					if (chemical || ptaPmdPia) {	
 						mCUNormsValueDTO.setSapCode(row[30] != null ? row[30].toString() : "");
 					}
 				}
