9286a22c5227e19dde6209729cae7d73335071b7
Kiran P <mindnerves.dev@mindnerves.com>
2026-09-21 20:35:22 +0530

    SSO login issue check

--- apps/react/case-portal/src/App.js
+++ apps/react/case-portal/src/App.js
@@ -34,8 +34,8 @@ const App = () => {
     const inIframe = window.self !== window.top
 
     keycloak.init({
-      onLoad: inIframe ? 'check-sso' : 'login-required',
-      // onLoad: inIframe ? 'login-required' : 'login-required',
+      // onLoad: inIframe ? 'check-sso' : 'login-required',
+      onLoad: inIframe ? 'login-required' : 'login-required',
       // checkLoginIframe: true,
       // silentCheckSsoRedirectUri: window.location.origin + '/silent-check-sso.html'
     }).then((authenticated) => {
